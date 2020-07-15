256d56f042900e2aace69a80406c4083f64bce82
Andrei Hoffman <intern.andrei.hoffman@assist.ro>
2020-07-15 09:49:00 +0300

    Set-up

---
+++ green/_local_settings.py
@@ -0,0 +1,14 @@
+USER = ""
+NAME=""
+PASSWORD=""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': NAME,
+        'USER': USER,
+        'PASSWORD': PASSWORD,
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
