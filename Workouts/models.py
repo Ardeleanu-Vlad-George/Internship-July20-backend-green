648afa3cbfab551baaca9fd8d8217e7f16e2eabb
blasciuc <blasciuc.gabriele@gmail.com>
2020-07-16 14:23:59 +0300

    com

--- Workouts/models.py
+++ Workouts/models.py
@@ -1,5 +1,6 @@
 
 from django.db import models
+from django.core.exceptions import ValidationError
 from user.models.py import Users
 
 
@@ -17,4 +18,23 @@ class Workouts(models.Model):
     calories_burned = models.DecimalField(decimal_places=2, max_digits=4)
     average_speed = models.DecimalField(decimal_places=2, max_digits=4)
     workout_effectiveness = models.BooleanField()
+    def clean_name(self):
+        if self.name=='':
+            raise ValidationError('Empty field')
+        if len(self.name)>30:
+            raise ValidationError('Input a correct name')
+    def clean_description(self):
+        if self.description=='':
+            raise ValidationError('Empty field')
+        if len(self.description)>50:
+            raise ValidationError('Input a correct description')
+    def clean_lat(self):
+        if
+
+
+
+
+
+
+
 # Create your models here.
