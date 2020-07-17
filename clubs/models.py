f418c40240a0c32c31bfe91e84352a988d52f046
Ardeleanu Vlad-George <ardeleanu_vlad@ymail.com>
2020-07-17 15:26:25 +0300

    basic_structure_club

--- clubs/models.py
+++ clubs/models.py
@@ -1,4 +1,13 @@
 from django.db import models
-from user.models import Users
+from user.models import Users, Sport
 
 # Create your models here.
+
+class Club(models.Model):
+    #id - autofield
+    #owner - fk to user
+    #name - charfield
+    #description - textfield
+    owner = models.ForeignKey(Users, on_delete=models.CASCADE())
+    name = models.CharField(max_length=51)
+    description = models.TextField()
