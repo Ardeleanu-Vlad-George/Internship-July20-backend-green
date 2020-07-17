f46fdd3f84313bb20c9c18a6f622f10be94a4a63
Ardeleanu Vlad-George <ardeleanu_vlad@ymail.com>
2020-07-17 15:33:21 +0300

    basic event structure done

--- clubs/models.py
+++ clubs/models.py
@@ -8,6 +8,23 @@ class Club(models.Model):
     #owner - fk to user
     #name - charfield
     #description - textfield
+
     owner = models.ForeignKey(Users, on_delete=models.CASCADE())
     name = models.CharField(max_length=51)
     description = models.TextField()
+
+class Event(models.Model):
+    #id - autofield
+    #club - fk to Club
+    #name - Charfield
+    #description - textfield
+    #location - charfield
+    #radius - charfield
+    #sport - foreignkey
+
+    org_club = models.ForeignKey(Club, on_delete=models.CASCADE())
+    name = models.CharField(max_length=51)
+    description = models.TextField()
+    location = models.CharField(max_length=31)
+    radius = models.CharField(max_length=31)
+    event_sport = models.ForeignKey(Sport, on_delete=models.CASCADE())
