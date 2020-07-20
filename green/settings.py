b57c59731950eed84c50696cb8b66b6cad988eae
Ardeleanu Vlad-George <ardeleanu_vlad@ymail.com>
2020-07-20 09:50:53 +0300

    finish

--- green/settings.py
+++ green/settings.py
@@ -105,6 +105,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
+
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
