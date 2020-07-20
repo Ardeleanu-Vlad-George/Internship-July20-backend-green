1475d8e47f66e0b856cf1c3c33850209ec0c5a59
Ardeleanu Vlad-George <ardeleanu_vlad@ymail.com>
2020-07-20 12:01:16 +0300

    reworked

--- user/models.py
+++ user/models.py
@@ -23,11 +23,7 @@ class Users(AbstractUser):
     age = models.IntegerField()
 
     def first_restriction(self):
-<<<<<<< HEAD
-
-=======
         pass
->>>>>>> vlad_club_event
     def clean_first(self):
         if self.first_name == '':
             raise ValidationError('Empty field')
