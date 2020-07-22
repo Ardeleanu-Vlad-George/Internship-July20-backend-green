04376f291a6f6eb8a1f3fc4c0b8f1d3c63633ab3
Ardeleanu Vlad-George <ardeleanu_vlad@ymail.com>
2020-07-22 10:36:37 +0300

    serializers

--- clubs/serializers.py
+++ clubs/serializers.py
@@ -1,3 +1,14 @@
-from clubs.models import Club, Event
 
-#class ClubSeria
+from rest_framework import serializers
+from clubs.models import Club, ClubHistory
+
+class ClubSerializer(serializers):
+    name = serializers.CharField(200)
+    description = serializers.CharField(400)
+
+    def create(self, validated_input):
+        return Club.objects.create(**validated_input)
+
+    def update(self, instance, validated_input):
+        instance.name = validated_input.get('name', instance.name)
+        instance.description = validated_input.get('description', instance.description)
